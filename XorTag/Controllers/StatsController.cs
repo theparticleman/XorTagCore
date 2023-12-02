40fab7cd30940bfa262326f0161b92934f31d24b
Jon Turner <theparticleman@gmail.com>
2023-12-02 16:23:28 -0700

    Output cache the stats endpoint and only request it ever 1 second on the frontend

--- XorTag/Controllers/StatsController.cs
+++ XorTag/Controllers/StatsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
 using XorTag.Commands;
 
 namespace XorTag.Controllers;
@@ -14,6 +15,7 @@ public class StatsController: ControllerBase
     }
 
     [Route("/stats")]
+    [OutputCache(Duration = 1, VaryByQueryKeys = [])]
     public StatsResult Get()
     {
         return statsCommand.Execute();
